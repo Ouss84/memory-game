5485eef0245033512da09b239dd53d40eafb80fe
Ouss Bahri <bahrioussama84@gmail.com>
2020-11-24 18:17:18 +0200

    add function to check two cards are flipped at the same time

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const cards = document.querySelectorAll(".card");
 
+//declaring global variables
+let isCardFlipped = false;
+let cardOne, cardTwo;
+
 //defining flipCard function
 function flipCard (){
- this.classList.toggle('is-flipped');
+ this.classList.add('is-flipped');
+
+ if (!isCardFlipped)
+ //first click
+ {isCardFlipped = true;
+ cardOne = this;
+ } else {
+     isCardFlipped =false;
+     cardTwo = this;
+     console.log ({cardOne,cardTwo})
+ }
 }
 
 //looping through all the cards
